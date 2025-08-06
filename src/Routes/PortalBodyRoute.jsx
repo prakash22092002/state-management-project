3518344acce2b3663d6a2a133455ef6658828202
Prakash Paudel <p.prakashpaudel35@gmail.com>
2025-08-06 09:55:37 +0530

    feat: add Customer, dashboard and order components and update navbar with routes

--- src/Routes/PortalBodyRoute.jsx
+++ src/Routes/PortalBodyRoute.jsx
@@ -4,9 +4,11 @@ import SuspenseFallbackCompo from "../Pages/SuspenseFallbackCompo";
 
 // lazy loading
 const Home = lazy(() => import("../Pages/Home"));
-const About = lazy(() => import("../Pages/About"));
-const Cart = lazy(() => import("../Pages/Cart"));
+const Dashboard = lazy(() => import("../Pages/Dashboard"))
 const Products = lazy(() => import("../Pages/Products"));
+const Orders = lazy(() => import("../Pages/Orders"))
+const Customers = lazy(() => import("../Pages/Customers"))
+const About = lazy(() => import("../Pages/About"));
 
 const PortalBodyRoute = () => {
 
@@ -20,9 +22,11 @@ const PortalBodyRoute = () => {
                 {/* for the top level routes */}
                 <Route path="">
                     <Route index element={<Home />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/products" element={<Products />} />
+                    <Route path="/orders" element={<Orders />} />
+                    <Route path="/customers" element={<Customers />} />
                     <Route path="/about" element={<About />} />
-                    <Route path="/cart" element={<Cart />} />
                 </Route>
             </Routes>
         </Suspense>
