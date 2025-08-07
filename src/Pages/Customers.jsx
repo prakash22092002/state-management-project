1e4410e71096ecbf3d44d422b8cd694db79747e4
Prakash Paudel <p.prakashpaudel35@gmail.com>
2025-08-07 23:32:14 +0530

    feat: added the list view for the customers and orders using hte dynamic component.

--- src/Pages/Customers.jsx
+++ src/Pages/Customers.jsx
@@ -1,8 +1,58 @@
+import usersQuery from "../Queries/usersQuery"
+import ListViewTable from "../Component/ListViewTable";
+
 const Customers = () => {
+    const { usersData, usersDataLoading } = usersQuery();
+
+    const customerListViewTableData = [
+        {
+            columnName: "ID",
+            keyName: "id",
+            styling: "flex-[0.5] truncate", // ~5%
+        },
+        {
+            columnName: "First Name",
+            keyName: "first_name",
+            styling: "flex-[1.5] truncate", // ~10%
+        },
+        {
+            columnName: "Last Name",
+            keyName: "last_name",
+            styling: "flex-[1.5] truncate", // ~10%
+        },
+        {
+            columnName: "Email",
+            keyName: "email",
+            styling: "flex-[3] truncate", // ~30%
+        },
+        {
+            columnName: "Phone",
+            keyName: "phone",
+            styling: "flex-[1.5] truncate", // ~15%
+        },
+        {
+            columnName: "Location",
+            keyName: "location",
+            styling: "flex-[2] truncate", // ~20%
+        },
+        {
+            columnName: "Created At",
+            keyName: "created_at",
+            styling: "flex-[1] truncate", // ~10%
+        },
+    ]
+
+
     return (
-        <div>
-            <p>Customers page</p>
-        </div>
+        <section className="users-section">
+            {/* this is the list table component dynamic */}
+            <ListViewTable
+                tableData={usersData}
+                isLoading={usersDataLoading}
+                columnConfig={customerListViewTableData}
+            />
+        </section>
     )
 }
+
 export default Customers
--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -1,8 +1,64 @@
+import ordersQuery from "../Queries/ordersQuery"
+import ListViewTable from "../Component/ListViewTable";
+
 const Orders = () => {
+    const { ordersData, ordersDataLoading } = ordersQuery();
+    console.log(ordersData, "ordersData")
+
+    const orderListViewTableData = [
+        {
+            columnName: "Order ID",
+            keyName: "id",
+            styling: "flex-[0.5] truncate",
+        },
+        {
+            columnName: "Product",
+            keyName: "product_name",
+            styling: "flex-[2] truncate",
+        },
+        {
+            columnName: "Category",
+            keyName: "category",
+            styling: "flex-[1.5] truncate",
+        },
+        {
+            columnName: "Quantity",
+            keyName: "quantity",
+            styling: "flex-[1] truncate",
+        },
+        {
+            columnName: "Price",
+            keyName: "price",
+            styling: "flex-[1.5] truncate after:content-['_USD']",
+        },
+        {
+            columnName: "User ID",
+            keyName: "user_id",
+            styling: "flex-[1] truncate",
+        },
+        {
+            columnName: "Status",
+            keyName: "status",
+            styling: "flex-[1.2] truncate capitalize",
+        },
+        {
+            columnName: "Order Date",
+            keyName: "order_date",
+            styling: "flex-[1.5] truncate",
+        },
+    ];
+
+
     return (
-        <div>
-            <p>Orders page</p>
-        </div>
+        <section className="orders-section ">
+            {/* this is dynamic list component */}
+            <ListViewTable
+                tableData={ordersData}
+                isLoading={ordersDataLoading}
+                columnConfig={orderListViewTableData}
+            />
+
+        </section>
     )
 }
 
