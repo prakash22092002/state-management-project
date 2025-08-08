c43bbf0274d92d6dbd36ad973af7e23efa2588dc
Prakash Paudel <p.prakashpaudel35@gmail.com>
2025-08-08 15:21:31 +0530

    feat(customer): added teh querry for the customer to handle the data

---
+++ src/Queries/customersQuery.jsx
@@ -0,0 +1,43 @@
+import { useQuery } from "@tanstack/react-query"
+import { fetchingApi } from "../utils/api"
+import { useLocation } from "react-router-dom"
+
+const customersQuery = () => {
+
+    const { pathname } = useLocation();
+    console.log(pathname);
+
+    // fetch all customers
+    const {
+        data: customersData,
+        isLoading: customersDataLoading,
+        error: customersDataError,
+    } = useQuery({
+        queryKey: ['customers'],
+        queryFn: () => fetchingApi(`https://6892dc21c49d24bce868893a.mockapi.io/portal/customers`),
+    })
+
+
+    // fetch particularcustomer
+    const {
+        data: customerData,
+        isLoading: customerDataLoading,
+        error: customerDataError,
+    } = useQuery({
+        queryKey: ['customer', pathname],
+        queryFn: () => fetchingApi(`https://6892dc21c49d24bce868893a.mockapi.io/portal${pathname}`),
+    })
+
+
+
+    return {
+        customersData,
+        customersDataLoading,
+        customersDataError,
+        customerData,
+        customerDataLoading,
+        customerDataError
+    };
+
+}
+export default customersQuery
